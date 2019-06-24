c22b71805c24704469e2edc31cfefbf00c398e4f
Jeffrey Marks <JKDMarks@gmail.com>
2019-06-24 17:50:58 -0400

    Delete extraneous

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Navbar, Nav, Dropdown } from 'react-bootstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar bg="light">
+          <Navbar.Brand>Alexandria</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse>
+            <Nav className="justify-content-end" style={{width: "100%"}}>
+              <Nav.Item>
+                Hello
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            Dropdown Button
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item onClick={() => console.log("#/action-1")}>Action</Dropdown.Item>
+            <Dropdown.Item onClick={() => console.log("#/action-2")}>Another action</Dropdown.Item>
+            <Dropdown.Item onClick={() => console.log("#/action-3")}>Something else</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+
+      </div>
+    )
+  }
 }
-
-export default App;
