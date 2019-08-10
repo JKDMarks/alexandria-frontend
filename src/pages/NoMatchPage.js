1bd666ef0e689dfa968a458e3b7096e40756b57d
Jeffrey Marks <JKDMarks@gmail.com>
2019-08-10 13:41:14 -0400

    Totally lost image

--- src/pages/NoMatchPage.js
+++ src/pages/NoMatchPage.js
@@ -6,7 +6,7 @@ class NoMatchPage extends Component {
   render() {
     return (
       <div style={{textAlign: "center"}}>
-        <img src="https://img.scryfall.com/cards/art_crop/en/gtc/54.jpg?1517813031" alt="Totally Lost"/>
+        <img src="https://img.scryfall.com/cards/art_crop/front/e/c/ec8e4142-7c46-4d2f-aaa6-6410f323d9f0.jpg?1561851198" alt="Totally Lost"/>
         <br/>
         <Link to="/">Go Home =></Link>
       </div>
